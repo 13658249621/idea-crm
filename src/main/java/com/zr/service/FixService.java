392eef295a391fe418078de5d73d50b43dd2d9f8
一只野生的提莫酱 <48589970+13658249621@users.noreply.github.com>
2020-03-31 00:07:28 +0800

    Update FixService.java

--- src/main/java/com/zr/service/FixService.java
+++ src/main/java/com/zr/service/FixService.java
@@ -3,9 +3,10 @@ package com.zr.service;
 import com.zr.dao.Dao;
 
 public class FixService {
-    private Dao dao=new Dao();
-    public boolean addFixService(String danyuan,String louceng,String fangjian,String date) {
+    private Dao dao = new Dao();
 
-        return dao.addFix(danyuan,louceng,fangjian,date);
+    public boolean addFixService(String danyuan, String louceng, String fangjian, String date, String uname, String phone, String info,  String state) {
+
+        return dao.addFix(danyuan, louceng, fangjian, date, uname,phone,info,state );
     }
 }
